30cf4c1a94c2402d7945474d12776a0a91f522af
Daniil Loyko <loikozavr@yandex.ru>
2024-11-02 22:44:24 +0300

    add object disappearing

---
+++ Assets/CodeBase/ThrowableObjects/DisappearableObject.cs
@@ -0,0 +1,54 @@
+using CodeBase.Infrastructure.StaticData;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.ThrowableObjects
+{
+    public class DisappearableObject : MonoBehaviour
+    {
+        [SerializeField] private DisappearableObjectStaticData _staticData;
+
+        private Color _originColor;
+        private SpriteRenderer _spriteRenderer;
+        private Coroutine _disappearCoroutine;
+
+        public bool IsDisappearing => _disappearCoroutine != null;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _originColor = _spriteRenderer.color;
+        }
+
+        public void StartDisappear()
+        {
+            _disappearCoroutine = StartCoroutine(DisappearRoutine());
+        }
+
+        public void StopDisappear()
+        {
+            StopCoroutine(_disappearCoroutine);
+            _spriteRenderer.color = _originColor;
+        }
+
+        private IEnumerator DisappearRoutine()
+        {
+
+            float disappearTime = 0;
+            bool isClear = false;
+
+            while (disappearTime < _staticData.DisappearTime)
+            {
+                _spriteRenderer.color = isClear ? _originColor : Color.clear;
+                isClear = !isClear;
+
+                yield return new WaitForSeconds(_staticData.TimeBetweenFlashes);
+                disappearTime += _staticData.TimeBetweenFlashes;
+            }
+
+            _spriteRenderer.color = _originColor;
+            gameObject.SetActive(false);
+        }
+    }
+}
