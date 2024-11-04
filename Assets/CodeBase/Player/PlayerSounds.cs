7208fdc5b03c872eed05d378086d85394e192817
Daniil Loyko <loikozavr@yandex.ru>
2024-11-04 12:29:22 +0300

    add sounds

---
+++ Assets/CodeBase/Player/PlayerSounds.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Player
+{
+    public class PlayerSounds : MonoBehaviour
+    {
+        [SerializeField] private List<AudioClip> _throwSounds;
+        [SerializeField] private List<AudioClip> _stepSounds;
+
+        public void PlayThrowClip()
+        {
+
+        }
+        public void PlayStepClip()
+        {
+
+        }
+    }
+}
