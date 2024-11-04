37616659227fe43a232800f87ef2aec9b9aa4213
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-11-04 17:37:34 +0300

    fix

--- Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -13,8 +13,6 @@ namespace CodeBase.Infrastructure
         {
             _game = new Game(this, Instantiate(_curtainPrefab));
             _game.StateMachine.Enter<BootstrapState>();
-
-            DontDestroyOnLoad(this);
         }
     }
 }
