25af4b40daf14f9edd3edde89e58b89b5e764f47
DukeTime <duke.of.times@gmail.com>
2024-11-04 18:55:00 +0300

    debug

--- Assets/Scripts/UISample/SoundSlider.cs
+++ Assets/Scripts/UISample/SoundSlider.cs
@@ -16,17 +16,20 @@ public class SoundSlider : MonoBehaviour
         _music.onValueChanged.AddListener(delegate { SliderMusicChange(); });
         _volume.onValueChanged.AddListener(delegate { SliderVolumeChange(); });
         _ambient.onValueChanged.AddListener(delegate { SliderAmbientChange(); });
+        SliderMusicChange();
+        SliderVolumeChange();
+        SliderAmbientChange();
     }
     public void SliderMusicChange()
     {
-        _mixerGroup.audioMixer.SetFloat("Music", Mathf.Lerp(-80, 0, _music.value));
+        _mixerGroup.audioMixer.SetFloat("Music", Mathf.Lerp(-80, 20, _music.value));
     }
     public void SliderVolumeChange()
     {
-        _mixerGroup.audioMixer.SetFloat("Effects", Mathf.Lerp(-80, 0, _volume.value));
+        _mixerGroup.audioMixer.SetFloat("Effects", Mathf.Lerp(-80, 20, _volume.value));
     }
     public void SliderAmbientChange()
     {
-        _mixerGroup.audioMixer.SetFloat("Ambient", Mathf.Lerp(-80, 0, _ambient.value));
+        _mixerGroup.audioMixer.SetFloat("Ambient", Mathf.Lerp(-80, 20, _ambient.value));
     }
 }
